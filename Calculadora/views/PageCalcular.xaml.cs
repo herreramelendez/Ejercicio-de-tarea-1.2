5bd2184550e981e7600606d8495dd587ef28f063
herreramelendez <127003756+herreramelendez@users.noreply.github.com>
2023-03-04 23:11:30 -0600

    Add files via upload

---
+++ Calculadora/views/PageCalcular.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculadora.views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculadora.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageCalcular : ContentPage
+    {
+        public PageCalcular()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnsuma_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("SUMA",Convert.ToString(Convert.ToDouble(numero1.Text) + Convert.ToDouble(numero2.Text)), "OK");
+        }
+
+        private async void btnResta_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("RESTA", Convert.ToString(Convert.ToDouble(numero1.Text) - Convert.ToDouble(numero2.Text)), "OK");
+        }
+
+        private async void btnMulti_Clicked(object sender, EventArgs e)
+        {
+           await DisplayAlert("MULTIPLICACION", Convert.ToString(Convert.ToDouble(numero1.Text) * Convert.ToDouble(numero2.Text)), "OK");
+        }
+
+        private async void btnDiv_Clicked(object sender, EventArgs e)
+        {
+          await DisplayAlert("DIVISION", Convert.ToString(Convert.ToDouble(numero1.Text) / Convert.ToDouble(numero2.Text)), "OK");
+            
+        }
+
+       
+    }
+}
